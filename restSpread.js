9689268713682f4aeaaf2d57a7e5a717dfaf28dd
rossg2021 <77760650+rossg2021@users.noreply.github.com>
2021-03-04 21:19:15 -0500

    Add files via upload

---
+++ restSpread.js
@@ -0,0 +1,27 @@
+// function filterOutOdds() {
+//     var nums = Array.prototype.slice.call(arguments);
+//     return nums.filter(function(num) {
+//       return num % 2 === 0
+//     });
+//   }
+  
+
+// const filterOutOdds = (...args) => 
+
+
+const findMin = (...args) => Math.min(...args)
+
+
+findMin(1,4,12,-3) // -3
+findMin(1,-1) // -1
+findMin(3,1) // 1
+
+mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
+
+const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2})
+
+
+
+doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
+doubleAndReturnArgs([2],10,4) // [2, 20, 8]
+
